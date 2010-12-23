00df4510e587aa73a399c01365e82e38692706e2
Nathan Vander Wilt <nate@calftrail.com>
2010-12-23 11:06:38 -0800

    updated atom generation to be more consistent with storage format to simplify code

--- lists/posts.js
+++ lists/posts.js
@@ -14,7 +14,7 @@ function(head, req) {
     provides("atom", function() {
         // copied liberally from https://github.com/jchris/sofa/blob/master/lists/index.js
         var Atom = require("lib/atom");
-        var newDate = require("lib/date").newDate;
+        var toRFC3339 = require("lib/date").toRFC3339;
         
         function postPath(id) {
             // after https://github.com/couchapp/couchapp/blob/master/vendor/lib/path.js
@@ -24,31 +24,18 @@ function(head, req) {
         
         // load the first row to find the most recent change date
         var row = getRow();
-        var feedHeader = Atom.header({
-            feed_id: ddoc.blog.uri,
-            title: ddoc.blog.title,
-            updated: (row ? newDate(row.doc.published) : new Date()),
-            
-            author: ddoc.blog.author,
-            feed_link: path.absolute(feedPath)
-        });
-        send(feedHeader);
+        var blog = ddoc.blog;
+        blog.updated = (row) ? (row.doc.updated || row.doc.published) : toRFC3339(new Date());
+        blog.self = path.absolute(feedPath);
+        send(Atom.header(blog));
         
         while (row) {
-            var feedEntry = Atom.entry({
-                entry_id: row.doc.uri,
-                title: row.doc.title,
-                published: newDate(row.doc.published),
-                updated: row.doc.updated && newDate(row.doc.updated),
-                
-                html_content: row.doc.html_content,
-                // TODO: also use as xml:base so images and such work?
-                alternate: path.absolute(path.show('post', row.id))
-            });
-            send(feedEntry);
+            var post = row.doc;
+            post.alternate = path.absolute(path.show('post', row.id));
+            send(Atom.entry(post));
             row = getRow();
         }
-            
-        send("</feed>");
+        
+        send(Atom.footer());
     });
 }
