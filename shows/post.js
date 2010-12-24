4b62aeb2914ddb61a10373788b5676794347c87d
Nathan Vander Wilt <nate@calftrail.com>
2010-12-23 18:59:07 -0700

    post pages now have specific titles

--- shows/post.js
+++ shows/post.js
@@ -2,5 +2,5 @@ function(doc, req) {
   var ddoc = this;
   var Mustache = require("lib/mustache");
   var postToTheme = require("lib/glob").postToTheme;
-  return Mustache.to_html(ddoc.templates.theme, {post:postToTheme(doc)}, ddoc.templates.partials);
+  return Mustache.to_html(ddoc.templates.theme, {title:doc.title, post:postToTheme(doc)}, ddoc.templates.partials);
 }
