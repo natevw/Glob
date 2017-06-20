9c8293da62a3a6cf552653106a30d64a545bbaf5
Nathan Vander Wilt <nate@calftrail.com>
2017-06-20 16:58:39 -0700

    fix by_date view by symlinking in library to views

--- views/by_date/map.js
+++ views/by_date/map.js
@@ -2,9 +2,6 @@ function(doc) {
     if (doc.type != "http://stemstorage.net/glob/post") return;
     if (!doc.published) return;
     
-    var exports = {};
-    // !code lib/date.js
-    var date = exports;
-    
+    var date = require("views/lib/date");
     emit(date.toUTCComponents(date.newDate(doc.published)));
 }
